f7274f47162c2ed3c8606f238467beb1908577c9
Riccardo Gucciardo <riccardogucciardo003@gmail.com>
2025-02-06 12:50:43 +0100

    names,hobbie

---
+++ hobbie.js
@@ -0,0 +1,15 @@
+//2 - hobbies.js
+//1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire  
+// un array di stringhe contenenti gli hobby.
+//2. Esporta la funzione dal file.
+function hobbies(hobbie1,hobbie2,hobbie3){
+    return[hobbie1,hobbie2,hobbie3]
+    
+    
+        
+    
+
+}
+
+export{hobbies}
+console.log(hobbies("calcio","pallavolo","basket"))
